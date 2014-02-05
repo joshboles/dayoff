cce044ea1b4fc673461772ee11ea9e87e70c8ad6
Josh Boles <jaboles@gmail.com>
2014-02-04 23:44:39 -0500

    Hook up intent for Settings activity.

--- app/src/main/java/com/joshboles/dayoff/MainActivity.java
+++ app/src/main/java/com/joshboles/dayoff/MainActivity.java
@@ -1,5 +1,6 @@
 package com.joshboles.dayoff;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -39,6 +40,8 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
