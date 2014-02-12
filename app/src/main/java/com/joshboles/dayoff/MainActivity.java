30b5381a19c5df091cc3a8945f2145afae0f9f50
Josh Boles <jaboles@gmail.com>
2014-02-12 11:41:08 -0500

    Remove call to Settings Activity

--- app/src/main/java/com/joshboles/dayoff/MainActivity.java
+++ app/src/main/java/com/joshboles/dayoff/MainActivity.java
@@ -1,6 +1,5 @@
 package com.joshboles.dayoff;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -42,8 +41,6 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
