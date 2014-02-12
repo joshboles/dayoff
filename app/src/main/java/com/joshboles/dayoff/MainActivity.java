62b22ea8fed523788e6d78bb1ef5219e81031fac
Josh Boles <jaboles@gmail.com>
2014-02-12 12:02:31 -0500

    Tie in Contact and Message activities

--- app/src/main/java/com/joshboles/dayoff/MainActivity.java
+++ app/src/main/java/com/joshboles/dayoff/MainActivity.java
@@ -1,5 +1,6 @@
 package com.joshboles.dayoff;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -40,7 +41,14 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_contacts) {
+            Intent intent = new Intent(this, ContactActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_messages) {
+            Intent intent = new Intent(this, MessageActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
